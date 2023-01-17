4a44f9a71c634fc002ef64e5d7f36717f993ccb3
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-17 15:49:57 -0500

    Initial Commit

---
+++ platzi/platziApp.swift
@@ -0,0 +1,17 @@
+//
+//  platziApp.swift
+//  platzi
+//
+//  Created by Jairo Jair Toro Novellis on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct platziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
